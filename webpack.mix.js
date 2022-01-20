76105a585260819e060b8b7bcf8f419ea7020f4e
Ben Balcombe <52495845+BenBalcombe19@users.noreply.github.com>
2022-01-20 21:06:19 +0000

    website blocker extension

---
+++ webpack.mix.js
@@ -0,0 +1,9 @@
+let mix = require('laravel-mix');
+
+mix.setPublicPath('./')
+    .sass('src/scss/popup.scss', 'dist/css')
+    .js('src/js/background.js', 'dist/js')
+    .js('src/js/popup.js', 'dist/js').vue()
+    .options({
+        processCssUrls:false
+    });
