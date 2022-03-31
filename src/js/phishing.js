b920b8528593f77973afda198b056b3669841e7d
Ben Balcombe <52495845+BenBalcombe19@users.noreply.github.com>
2022-03-31 16:27:24 +0100

    updated subject analysis

--- src/js/phishing.js
+++ src/js/phishing.js
@@ -4,6 +4,11 @@ var sentiment = new Sentiment();
 
 class Phishing {
     constructor(){
+        this.scores = {
+            address: 1,
+            name: 1,
+            subject: 1
+        }
         this.phishy = false;
     }
 
@@ -15,7 +20,38 @@ class Phishing {
     }
     
     validateSubject(subject){
-        console.log("Validating email subject")
+        let urgencyCounter = 0;
+        let urgencyWords = ["now","hurry","quick","limited","urgent","urgently","important"];
+        let tokenisedSubject = subject ? subject.toLowerCase().split(" ") : "";
+        let containsSpecialCharacter = false
+        let regex = new RegExp('[!#$%^&*(){}|<>]');
+
+        
+        tokenisedSubject.forEach(word => {
+            if (urgencyWords.includes(word)){
+                urgencyCounter++;
+            }
+        });
+        
+        containsSpecialCharacter = regex.test(subject);
+
+        if (containsSpecialCharacter){
+            if (urgencyCounter < 2){
+                this.scores.subject = 4;
+            }  else {
+                this.scores.subject = 5;
+            }
+        } else {
+            if (urgencyCounter != 0){
+                if (urgencyCounter == 1){
+                    this.scores.subject = 3;
+                } else if (urgencyCounter == 2){
+                    this.scores.subject = 4;
+                } else {
+                    this.scores.subject = 5;
+                }
+            }
+        }
         // var result = sentiment.analyze(subject);
         // console.log("Sentiment result:",result);
         let urgencyWords = ["now","hurry","quick","limited","important"];
