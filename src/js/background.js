1281f6ba06d84e0242fdc59cf82ec66acb0869ce
Ben Balcombe <52495845+BenBalcombe19@users.noreply.github.com>
2022-04-06 12:23:16 +0100

    background script data handling updates

--- src/js/background.js
+++ src/js/background.js
@@ -1,20 +1,28 @@
 // when the extension is first installed, set default values
-chrome.runtime.onInstalled.addListener(function() {
+chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.local.set({
+        active: true,
         emailData: {},
-        active: true
-    }, function() {});
+        activeWarning: false
+    }, function () { });
 });
 
-chrome.runtime.onMessage.addListener(function(msg, sender, response) {
+chrome.runtime.onMessage.addListener(function (msg, sender, response) {
     console.log('Message Received', msg)
-    if (msg.sender === 'content-script'){
-        if (msg.type === 'new-email'){
-            chrome.storage.local.set({
-                emailData: msg.data
-            }, () => {
-                chrome.runtime.sendMessage({sender:'background-script',type: 'data-update'});
-            });
+    if (msg.sender === 'content-script') {
+        if (msg.type === 'new-email') {
+            updateData(msg.data, 'data-update')
+        } else if (msg.type === 'no-data') {
+            updateData({}, 'data-update')
         }
     }
 })
+
+function updateData(data, message) {
+    chrome.storage.local.set({
+        emailData: data
+    }, () => {
+        chrome.runtime.sendMessage({ sender: 'background-script', type: 'data-update' });
+        console.log('data updated')
+    });
+}
