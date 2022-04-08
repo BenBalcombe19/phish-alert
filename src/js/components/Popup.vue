00a03b1bd53a40eed9a96159c8193c9562a23943
Ben Balcombe <52495845+BenBalcombe19@users.noreply.github.com>
2022-04-09 00:13:43 +0100

    popup component updated to new styling and analysis data

--- src/js/components/Popup.vue
+++ src/js/components/Popup.vue
@@ -1,13 +1,19 @@
 <template>
-    <div class="popup-wrapper">
-        <h1 v-if="isData" class="title">Current Email</h1>
+    <div class="popup-wrapper" :class="{ 'no-email-settings-open': (settings.show && (!isData || !extensionActive))}">
+        <div v-if="isData && extensionActive" class="title">
+            <div class=title-text>Email Rating:</div>
+            <rating v-if="isData" :riskValue="currentMailData.overallRating"></rating>
+        </div>
         
         <h1 v-else class="title center">Sorry, no email found</h1>
 
+        
         <span class="options-wrapper">
             <settings :show="settings.show" :title="settings.title" @close-popup="settings.show = false"></settings>
             
-            <i class="settings-cog-main fa-solid fa-gear" @click="settings.show = !settings.show"></i>
+            <i class="menu-button fa-solid fa-gear" @click="settingsInfoMechanic('settings')"></i>
+
+            <i class="menu-button fa-solid fa-circle-question" @click="settingsInfoMechanic('mainInfo')"></i>
 
             <label class="switch">
                 <input type="checkbox" v-model="extensionActive">
@@ -18,13 +24,12 @@
             </label>
             
         </span>
+
+        <info :show="mainInfo.show" :title="mainInfo.title" :data="mainInfo.data" :inTable="false" @close-popup="mainInfo.show = false"></info>
         
-
-
-
         <!-- EMAIL DETAILS TABLE -->
-        <div v-if="isData" class="table-title">General Details:</div>
-        <div v-if="isData" class="table">
+        <div v-if="isData && extensionActive" class="table-title">General Details:</div>
+        <div v-if="isData && extensionActive" class="table">
             <div class="row header" :class="{ disabled: !extensionActive}">
                 <div class="cell">Details</div>
                 <div class="cell value">Content</div>
@@ -34,14 +39,10 @@
 
             <div class="row">
                 <div class="cell">Sender Address</div>
-                <div class="cell value"><abbr :title="currentMailData.from.address">{{currentMailData.from.address}}</abbr></div>
+                <div class="cell value"><abbr :title="currentMailData.from.address" v-html="textHighlight(currentMailData.from.address, currentMailData.from.address.split('@').pop())">{{currentMailData.from.address}}</abbr></div>
                 <div class="cell"><rating :riskValue="fromAddressScore"></rating></div>
                 <div class="cell">
-                    <span @click="address.show = ! address.show" class="learn-button" :class="{ 'disabled-text': !extensionActive}">
-                        Learn More 
-                        <i v-if="!address.show" class="fa-solid fa-circle-info"></i>
-                        <i v-else class="fa-solid fa-chevron-up"></i>
-                    </span>
+                    <span @click="address.show = ! address.show" class="learn-button" :class="{ 'disabled-text': !extensionActive}">Learn More <i class="fa-solid fa-circle-info"></i></span>
                 </div>
             </div>
 
@@ -72,14 +73,14 @@
         </div>
 
         <!-- LINKS TABLE -->
-        <div v-if="isData && currentMailData.links.length != 0" class="table-title">Link Details 
+        <div v-if="isData && extensionActive && currentMailData.links.length != 0" class="table-title">Link Details 
             <span @click="linkInfo.show = ! linkInfo.show" class="learn-button" :class="{ 'disabled-text': !extensionActive}">
                 Learn More <i class="fa-solid fa-circle-info"></i>
             </span>
         </div>
         <info :show="linkInfo.show" :title="linkInfo.title" :data="linkInfo.data" :inTable="false" @close-popup="linkInfo.show = false"></info>
 
-        <div v-if="isData && currentMailData.links.length != 0" class="table link-table">
+        <div v-if="isData && extensionActive && currentMailData.links.length != 0" class="table link-table">
 
             <div class="row header sticky" :class="{ disabled: !extensionActive}">
                 <div class="cell">Link Text</div>
@@ -90,7 +91,7 @@
 
             <div class="row" v-for="link in currentMailData.links" :key="link.id">
                 <div class="cell"><abbr :title="link.innerText">{{link.innerText}}</abbr></div>
-                <div class="cell value"><abbr :title="link.href" v-html="domainHighlightURL(link.href,link.hostname)"></abbr></div>
+                <div class="cell value"><abbr :title="link.href" v-html="textHighlight(link.href,link.hostname)"></abbr></div>
                 <div class="cell tick-cross">
                     <i v-if="link.senderDomainDisparity" class="fa-solid fa-circle-xmark"></i>
                     <i v-else class="fa-solid fa-circle-check"></i>
@@ -102,29 +103,30 @@
         </div>
 
         <!-- ATTACHMENTS TABLE -->
-        <div v-if="isData && currentMailData.attachments.length != 0" class="table-title">Attachment(s) Details 
+        <div v-if="isData && extensionActive && currentMailData.attachmentsRated.length != 0" class="table-title">Attachment(s) Details 
             <span @click="attachmentInfo.show = ! attachmentInfo.show" class="learn-button" :class="{ 'disabled-text': !extensionActive}">
                 Learn More <i class="fa-solid fa-circle-info"></i>
             </span>
         </div>
         <info :show="attachmentInfo.show" :title="attachmentInfo.title" :data="attachmentInfo.data" :inTable="false" @close-popup="attachmentInfo.show = false"></info>
 
-        <div v-if="isData && currentMailData.attachments.length != 0" class="table link-table">
+        <div v-if="isData && extensionActive && currentMailData.attachmentsRated.length != 0" class="table link-table">
 
             <div class="row header sticky" :class="{ disabled: !extensionActive}">
                 <div class="cell">File Name</div>
-                <div class="cell value">Type</div>
+                <!-- <div class="cell value">Type</div> -->
                 <div class="cell">Size</div>
-                <!-- <div class="cell">Risk Rating</div> -->
+                <div class="cell">Risk Rating</div>
             </div>
 
-            <div class="row" v-for="attachment in currentMailData.attachments" :key="attachment.attachment_id">
-                <div class="cell"><abbr :title="attachment.name">{{attachment.name}}</abbr></div>
-                <div class="cell value"><abbr :title="attachment.type">{{attachment.type}}</abbr></div>
+            <div class="row" v-for="attachment in currentMailData.attachmentsRated" :key="attachment.attachment_id">
+                <div class="cell"><abbr :title="attachment.fileName" v-html="textHighlight(attachment.fileName, attachment.fileName.split('.').pop())"></abbr></div>
+                <!-- <div class="cell value"><abbr :title="attachment.type">{{attachment.type}}</abbr></div> -->
                 <div class="cell">{{attachment.size}}</div>
-                <!-- <div class="cell">
+                <!-- <div class="cell">{{Math.ceil(attachment.size / 1000) + 'KB'}}</div> -->
+                <div class="cell">
                     <rating :riskValue="attachment.riskRating"></rating>
-                </div> -->
+                </div>
             </div>
         </div>
 
@@ -136,6 +138,8 @@
 import rating from './Rating.vue';
 import info from './Info.vue';
 import settings from './Settings.vue';
+const parser = require('tld-extract');
+
 
 export default {
     data() {
@@ -144,6 +148,12 @@ export default {
             list: [],
             currentMailData: {},
             isData: false,
+            mainInfo:{
+                show: false,
+                title: 'General Information',
+                data:['The purpose of this extension is to educate and spread awareness on the identification of phishing emails and where possible, provide warning to users of potential phishing attempts.',
+                'Phishing is a social engineering technique used by cyber criminals to steal personal and usually valuable information from victims by pretending to be a trusted organisation.']
+            },
             settings:{
                 show: false,
                 title: 'Settings',
@@ -151,7 +161,8 @@ export default {
             address:{
                 show: false,
                 title: 'Useful Tips',
-                data: ['Make sure you trust this email address before progressing any further', 'If this is from an organisation or business, make sure the domain is spelled correctly as often attackers will try and trick users e.g no-reply@goggle.com']
+                data: ['Make sure you trust this email address before progressing any further', 'If this is from an organisation or business, make sure the domain is spelled correctly as often attackers will try and trick users e.g no-reply@goggle.com',
+                'An email from a nonsensical address (for example, 535vukasimeru@gmail.com) is almost certainly something you shouldn\'t open.']
             },
             name:{
                 show: false,
@@ -173,8 +184,8 @@ export default {
             attachmentInfo:{
                 show: false,
                 title: 'Attachment Information',
-                data:['Link Text - This is the text of the link displayed to the user e.g "Click Here"','Link URL - This is the actual URL that you will be directed to when you click the link text e.g "https://google.com"',
-                    'Domain Match - This refers to whether the domain of the Link URL (Highlighted in Blue) matches the domain of the sender address e.g no-reply@google.com, the domain is google.com']
+                data:['Unfortunately, there are many attachments file extensions that could potentially run code on your computer and thus install malware. e.g .exe, .src, .doc, .xls... ',
+                'Often, dangerous file extensions are concealed in .zip files','In addition, cybercriminals like to hide malicious links in PDF']
             },
             icons: {
                 active: 'icons/icon-48x48.png',
@@ -242,8 +253,17 @@ export default {
                 this.extensionActive = data.extensionActive;
             });    
         },
-        domainHighlightURL(url,domain){
-            return url.replace(domain, '<span class="domain-highlight">' + domain + '</span>')
+        textHighlight(mainText,highlightText){
+            return mainText.replace(highlightText, '<span class="text-highlight">' + highlightText + '</span>')
+        },
+        settingsInfoMechanic(element){
+            if (element == 'settings'){
+                this.settings.show = !this.settings.show;
+                this.mainInfo.show = false;
+            } else if (element == 'mainInfo'){
+                this.mainInfo.show = !this.mainInfo.show;
+                this.settings.show = false;
+            }
         }
     },
     
@@ -257,6 +277,22 @@ export default {
                 }
             }
         })
+
+        chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
+            let url = tabs[0].url;
+            try {
+                let domain = parser.parse_host(url, {allowUnknownTLD : true}).domain;
+                if (!domain.includes('google.com/mail')){
+                    setTimeout(() => {
+                        this.isData = false
+                        this.currentMailData = {}
+                    },300)
+                }
+                } catch{
+                    console.log('Tab URL failed')
+                    return false;
+                }
+        });
     },
 }
 </script>
